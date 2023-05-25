5adf2b14ae7e7e4fb94a35b1bc5a2ffbbbc2be81
Norean <33429403+Norean@users.noreply.github.com>
2023-05-25 13:57:29 +0200

    Update Main.java

--- Dictionnaire/src/Main.java
+++ Dictionnaire/src/Main.java
@@ -20,33 +20,36 @@ public class Main {
 		fichier.RemplirDictionnaire(dico,"./Ressources/Dico.txt");
 		System.out.println("/*** Fin de remplissage du dictionnaire ***/");
 		
-		String choix = "";
+		int choix = 0;
 		
-		while (!choix.equalsIgnoreCase("exit")) {
-		System.out.println("1 - nombre de mots :\n2 - traitement de texte :\nEND - exit :");
-		choix = scanner.nextLine();
+		while (choix != 4) {
+		System.out.println("1 - nombre de mots :\n2 - afficher les X permiers mots du dictionnaire\n3 - traitement de texte :\n4 - exit :");
+		choix = scanner.nextInt();
         
-        	switch (choix.toLowerCase()) {
-        		case "nombre de mots":
+        	switch (choix) {
+        		case 1:
         			System.out.println("/*** Comptage en cours ... ***/");
         			System.out.println(dico.compterMots());
         			break;
-        		case "traitement de texte":
+        		case 2:
+        			System.out.println("Combien de mots voulez vous affichez ?");
+        			int X = scanner.nextInt();
+        			dico.afficherXMots(X);
+        			break;
+        		case 3:
         			System.out.println("/*** Traitement en cours ... ***/");
-        			String choix2 = "";
-        			while (!choix2.equalsIgnoreCase("exit")) {
-        				System.out.println("1 - choix du fichier\n2 - mettre en minuscule\n3 - recherche de nouveau mot\n");
-        				choix2 = scanner.nextLine();
-        				switch (choix2.toLowerCase()) {
-        					case "choix du fichier":
+        			int choix2 = 0;
+        			while (choix2 != 3) {
+        				System.out.println("1 - choix du fichier\n2 - recherche de nouveau mot\n3 - exit :");
+        				choix2 = scanner.nextInt();
+        				switch (choix2) {
+        					case 1:
         						System.out.println("Quel est le nom du fichier a traiter ?");
-        						nom += scanner.nextLine();
+        						nom += scanner.next();
         						txt.setTexte(fichier.LireFichier(nom));
         						break;
-        					case "mettre en minuscule":
+        					case 2:
         						txt.setTexte(txt.MajToMin());
-        						break;
-        					case "recherche de nouveau mot":
         						ArrayList<String> mots = txt.rechercheNouveauMot(dico);
         						for (String mot : mots) {
         				            System.out.println(mot);
